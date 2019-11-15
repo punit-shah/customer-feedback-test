f7730db61a0b3520f9404a90d98f9bc878c8c095
Punit Shah <punit@punitshah.dev>
2019-11-15 23:30:23 +0000

    Reset form on submit

--- src/Form.js
+++ src/Form.js
@@ -3,22 +3,50 @@ import Rating from './Rating';
 import './Form.css';
 
 const Form = ({ onSubmit }) => (
-  <form className="Form" onSubmit={onSubmit}>
+  <form
+    className="Form"
+    onSubmit={event => {
+      event.preventDefault();
+      onSubmit(event);
+      event.target.reset();
+    }}
+  >
     <h2 className="Form-title">Leave a comment</h2>
 
-    <label className="Form-label" htmlFor="name">Name</label>
+    <label className="Form-label" htmlFor="name">
+      Name
+    </label>
     <input className="Form-field" type="text" name="name" id="name" required />
 
-    <label className="Form-label" htmlFor="email">Email</label>
-    <input className="Form-field" type="email" name="email" id="email" required />
+    <label className="Form-label" htmlFor="email">
+      Email
+    </label>
+    <input
+      className="Form-field"
+      type="email"
+      name="email"
+      id="email"
+      required
+    />
 
-    <label className="Form-label" htmlFor="Rating-input-1">Rating</label>
+    <label className="Form-label" htmlFor="Rating-input-1">
+      Rating
+    </label>
     <Rating name="rating" />
 
-    <label className="Form-label" htmlFor="comment">Comment</label>
-    <textarea className="Form-field Form-field-comment" name="comment" id="comment" required />
+    <label className="Form-label" htmlFor="comment">
+      Comment
+    </label>
+    <textarea
+      className="Form-field Form-field-comment"
+      name="comment"
+      id="comment"
+      required
+    />
 
-    <button className="Form-submit" type="submit">Submit feedback</button>
+    <button className="Form-submit" type="submit">
+      Submit feedback
+    </button>
   </form>
 );
 
