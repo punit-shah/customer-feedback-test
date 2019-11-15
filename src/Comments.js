72ff57ef0c149963e2db96313584759ac451bc5a
Punit Shah <punit@punitshah.dev>
2019-11-15 21:31:40 +0000

    Make app responsive

--- src/Comments.js
+++ src/Comments.js
@@ -3,7 +3,7 @@ import './Comments.css';
 import Rating from './Rating';
 
 const Comments = ({ comments }) => (
-  <div className="Comments">
+  <section className="Comments">
     {comments.map(({ id, name, rating, comment }) => (
       <div className="comment" key={id}>
         <div className="comment-name">{name}</div>
@@ -11,7 +11,7 @@ const Comments = ({ comments }) => (
         <p className="comment-body">{comment}</p>
       </div>
     ))}
-  </div>
+  </section>
 );
 
 export default Comments;
